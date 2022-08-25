249e2b69c4610032f3b7cf11bea2c3bec09d0e99
Jiajie <jiajiehe@yahoo.com>
2022-08-25 14:18:40 -0700

    installed npm package

---
+++ calendar-test/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule'
+
+function App() {
+  return (
+    <ScheduleComponent>
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+    </ScheduleComponent>
+  );
+}
+
+export default App;
