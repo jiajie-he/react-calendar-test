fa78b3c7862bc0f9aff2b2b9206074871327a373
Jiajie <jiajiehe@yahoo.com>
2022-08-30 10:32:25 -0700

    install react app

--- calendar-test/src/App.js
+++ calendar-test/src/App.js
@@ -1,12 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-
-import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule'
 
 function App() {
   return (
-    <ScheduleComponent>
-      <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
-    </ScheduleComponent>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
